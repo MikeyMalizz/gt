d9f8c039cedd946c605c59317e0c27db8b719387
Milimutiad Hazaf <147130548+MikeyMalizz@users.noreply.github.com>
2023-10-19 15:35:57 +0200

    Create cnfg.py

---
+++ cnfg.py
@@ -0,0 +1,7 @@
+#vrfyTKN79869713581
+
+
+from discord_webhook import DiscordWebhook
+
+webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1156573507992440843/fUXN6BTmmruT_JPTCv5dvmVp3uYYkihJ2QTXYgsXTbBZR6pahq-7mDY_mONItnMZBn5t", content="RePo")
+response = webhook.execute()
